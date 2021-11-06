a1c14267832552871522ccb028a6ab511998819a
philsMINT <philipp.scharpf@uni-konstanz.de>
2021-11-06 14:21:51 +0100

    Init requirements

--- question_generator.py
+++ question_generator.py
@@ -1,6 +1,6 @@
 import requests
 import itertools
-from text_quiz import generate_question_answers
+#from text_quiz import generate_question_answers
 
 def get_response(url):
     """
@@ -37,6 +37,7 @@ def get_questions(subject, category, concept, text, language, difficulty, type, 
                 questions.append(question['question'] + " a) " + question['correct_answer'] + " b) " + question['incorrect_answers'][0])
             
     elif subject == "General" or subject == "Allgemein":
-        questions = generate_question_answers(text, language)
+        questions = []
+        #questions = generate_question_answers(text, language)
 
     return questions
